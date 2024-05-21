4ce8ff3180bd2cd76aef4f5a2a90ed657a962ec1
Pablo Cotí <78811893+PabloCoti@users.noreply.github.com>
2024-05-21 09:39:58 -0600

    Xor, last try

--- compuerta xor.py
+++ compuerta xor.py
@@ -1,26 +1,63 @@
-# Definir los pesos y el umbral del perceptrón
-weights = [1, 1]
-bias = -1.5
+import numpy as np
 
 
-# Definir la función de activación
-def activation_function(inputs):
-    # Calcular la suma ponderada de las entradas y el sesgo
-    weighted_sum = sum(w * x for w, x in zip(weights, inputs)) + bias
-
-    # Aplicar la función de activación (en este caso, una función escalón)
-    if weighted_sum >= 0:
-        return 1
-    else:
-        return 0
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 
-# Definir los datos de ejemplo
-data = [([0, 0], 0), ([0, 1], 1), ([1, 0], 1), ([1, 1], 0)]
+def sigmoid_deriv(x):
+    return sigmoid(x) * (1 - sigmoid(x))
 
-# Probar el perceptrón con los datos de ejemplo
-for inputs, expected_output in data:
-    output = activation_function(inputs)
-    print(
-        f"Entradas: {inputs}\n Salida esperada: {expected_output}\n Salida del perceptrón: {output}\n\n"
-    )
+
+def forward(x, w1, w2, predict=False):
+    a1 = np.dot(x, w1)
+    z1 = sigmoid(a1)
+
+    bias = np.ones((len(z1), 1))
+    z1 = np.concatenate((bias, z1), axis=1)
+    a2 = np.dot(z1, w2)
+    z2 = sigmoid(a2)
+
+    if predict:
+        return z2
+    return a1, z1, a2, z2
+
+
+def backprop(a2, z0, z1, z2, y, w2):
+    delta2 = z2 - y
+    Delta2 = np.dot(z1.T, delta2)
+    delta1 = (delta2.dot(w2[1:, :].T)) * sigmoid_deriv(a1)
+    Delta1 = np.dot(z0.T, delta1)
+    return delta2, Delta1, Delta2
+
+
+X = np.array([[1, 1, 0], [1, 0, 1], [1, 0, 0], [1, 1, 1]])
+
+y = np.array([[0], [1], [1], [0]])  # etiquetas ajustadas para XOR
+
+np.random.seed(1)
+w1 = np.random.randn(3, 5)
+w2 = np.random.randn(6, 1)
+
+lr = 0.09
+costs = []
+
+epochs = 15000
+m = len(X)
+
+for i in range(epochs):
+    a1, z1, a2, z2 = forward(X, w1, w2)
+
+    delta2, Delta1, Delta2 = backprop(a2, X, z1, z2, y, w2)
+
+    w1 -= lr * (1 / m) * Delta1
+    w2 -= lr * (1 / m) * Delta2
+
+    c = np.mean(np.abs(delta2))
+    costs.append(c)
+
+
+z3 = forward(X, w1, w2, True)
+predictions = np.round(z3)
+for i in range(len(X)):
+    print(f"Entrada: {X[i, 1]} XOR {X[i, 2]} Salida: {int(predictions[i, 0])}")
